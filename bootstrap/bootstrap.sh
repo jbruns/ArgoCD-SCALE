45752befdda91eee30f014b45798011d287bafd7
Jacob Bruns <jbruns@live.com>
2021-08-23 14:59:23 -0700

    initial commit

---
+++ bootstrap/bootstrap.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+kubectl port-forward svc/argocd-server -n argocd 8080:443 &
