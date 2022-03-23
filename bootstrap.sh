8908b7bd1d1912eeb196e3de9640339e3e11d03e
Jacob Bruns <jbruns@live.com>
2022-03-23 14:09:54 -0700

    bootstrap: remove applicationset (installed by argocd)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 adminpassword=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)
 
+# install ArgoCD
 helm dependency update argocd
-helm dependency update argocd-applicationset
 helm upgrade --install argocd argocd -n argocd --create-namespace --wait --timeout 120s --values globalValues.yaml
-helm upgrade --install argocd-applicationset argocd-applicationset -n argocd --wait --timeout 120s
 kubectl patch secret -n argocd argocd-secret -p '{"stringData": { "admin.password": "'$(htpasswd -bnBC 10 "" ${adminpassword} | tr -d ':\n')'"}}'
 
 echo "ArgoCD admin password has been set to: ${adminpassword}"
