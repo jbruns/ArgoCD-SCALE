3dbc514437355f5027d43b0ff0083be48054da02
buddhabum <46009223+buddhabum@users.noreply.github.com>
2023-03-03 09:33:23 -0800

    Adding bootstrap logic for checking helm/argo installed
    ChatGPT

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 adminpassword=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)
 
-# install ArgoCD
-helm dependency update argocd
-helm upgrade --install argocd argocd -n argocd --create-namespace --wait --timeout 120s --values globalValues.yaml
-kubectl patch secret -n argocd argocd-secret -p '{"stringData": { "admin.password": "'$(htpasswd -bnBC 10 "" ${adminpassword} | tr -d ':\n')'"}}'
+# check if helm is installed
+if ! command -v helm &> /dev/null; then
+  # install helm
+  curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+fi
 
+# check if argocd exists
+if ! helm ls -q --namespace argocd | grep -q '^argocd$'; then
+  # add argocd repo
+  helm repo add argo https://argoproj.github.io/argo-helm || true # Add the ArgoCD chart repository
+  helm repo update # Update the repositories
+
+  # install argocd
+  helm dependency update argocd # Update the dependencies
+  helm upgrade --install argocd argo/argocd -n argocd --create-namespace --wait --timeout 120s --values globalValues.yaml # Install or upgrade the ArgoCD release
+fi
+
+# update argocd password
+kubectl patch secret -n argocd argocd-secret -p '{"stringData": { "admin.password": "'$(htpasswd -bnBC 10 "" ${adminpassword} | tr -d ':\n')'"}}' # Patch the admin password of the ArgoCD secret in the argocd namespace
 echo "ArgoCD admin password has been set to: ${adminpassword}"
 
 # kubectl port-forward -n argocd svc/argocd-server 8080:80
